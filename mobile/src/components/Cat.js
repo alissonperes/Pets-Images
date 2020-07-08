2457f303d70ea45fb597a272ff8c6b2441f4410c
Alisson Peres <aperes.dev@gmail.com>
2020-07-08 18:51:30 -0300

    Loading icon, image draggable on Dog

--- mobile/src/components/Cat.js
+++ mobile/src/components/Cat.js
@@ -2,18 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { getCat } from "../actions";
 import { connect } from "react-redux";
-import { TouchableOpacity, Image, StyleSheet, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 
 const Cat = props => {
   const { cat, fetchCat } = props;
   const { fetched, fetching, error } = cat;
-  const [image, setImage] = useState({
-    uri: "https://reactjs.org/logo-og.png"
-  });
+  const [loaded, setLoaded] = useState(false);
+  const [image, setImage] = useState();
 
   useEffect(() => {
     if (!fetched && !fetching && error === null) {
+      setImage({ uri: "" });
       fetchCat();
+      setLoaded(false);
+    } else if (fetching) {
+      setLoaded(false);
     } else if (fetched) {
       setImage({ uri: cat.cat.url });
     }
@@ -28,13 +38,22 @@ const Cat = props => {
       <StatusBar style="auto"></StatusBar>
       <Image
         source={image}
+        onLoad={() => setLoaded(true)}
         style={{
           flex: 1,
           width: "100%",
           flexDirection: "column",
-          justifyContent: "space-between"
+          justifyContent: "space-between",
+          resizeMode: "contain"
         }}
       />
+      {loaded ? null : (
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          style={styles.indicator}
+        />
+      )}
       <TouchableOpacity style={styles.button} onPress={onPress}>
         <Text>SuperLike</Text>
       </TouchableOpacity>
@@ -54,6 +73,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDDDDD",
     padding: 10,
     marginBottom: 10
+  },
+  indicator: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: -1
   }
 });
 
--- mobile/src/components/Dog.js
+++ mobile/src/components/Dog.js
@@ -2,18 +2,45 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { getDog } from "../actions";
 import { connect } from "react-redux";
-import { TouchableOpacity, Image, StyleSheet, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  Animated,
+  PanResponder,
+  View
+} from "react-native";
 
 const Dog = props => {
   const { dog, fetchDog } = props;
   const { fetched, fetching, error } = dog;
-  const [image, setImage] = useState({
-    uri: "https://reactjs.org/logo-og.png"
+  const [loaded, setLoaded] = useState(false);
+  const [image, setImage] = useState();
+  const [pan, setPan] = useState(new Animated.ValueXY());
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([
+      null,
+      {
+        dx: pan.x,
+        dy: pan.y
+      }
+    ]),
+    onPanResponderRelease: (e, gesture) => {
+      Animated.spring(pan, { toValue: { x: 0, y: 0 } }).start();
+    }
   });
 
   useEffect(() => {
     if (!fetched && !fetching && error === null) {
+      setImage({ uri: "" });
       fetchDog();
+      setLoaded(false);
+    } else if (fetching) {
+      setLoaded(false);
     } else if (fetched) {
       setImage({ uri: dog.dog.url });
     }
@@ -26,15 +53,37 @@ const Dog = props => {
   return (
     <View style={styles.container}>
       <StatusBar style="auto"></StatusBar>
-      <Image
-        source={image}
-        style={{
-          flex: 1,
-          width: "100%",
-          flexDirection: "column",
-          justifyContent: "space-between"
-        }}
-      />
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[
+          pan.getLayout(),
+          {
+            flex: 1,
+            width: "100%",
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }
+        ]}
+      >
+        <Image
+          source={image}
+          onLoad={() => setLoaded(true)}
+          style={{
+            flex: 1,
+            width: "100%",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            resizeMode: "contain"
+          }}
+        />
+      </Animated.View>
+      {loaded ? null : (
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          style={styles.indicator}
+        />
+      )}
       <TouchableOpacity style={styles.button} onPress={onPress}>
         <Text>SuperLike</Text>
       </TouchableOpacity>
@@ -54,6 +103,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDDDDD",
     padding: 10,
     marginBottom: 10
+  },
+  indicator: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: -1
   }
 });
 
